d883339fd6e8f8a1aa264ef769975909ae172c6a
hayasam <hayasam@najah.edu>
2024-07-10 11:52:35 +0300

    cosmatic changes

--- promp.py
+++ promp.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request
 import os
 
-app = Flask(__name__)
+
+
+
 
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+
+
+
+        file=''
         file = request.files['file']
         if file:
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
