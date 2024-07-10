e41a307ecf0c1a2ba31489225824b34b8685cc39
hayasam <hayasam@najah.edu>
2024-07-10 11:48:52 +0300

    change from html to py

---
+++ promp.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            return 'File uploaded successfully'
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
